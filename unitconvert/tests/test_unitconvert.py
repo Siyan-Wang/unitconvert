73551d3c93bc230a0195a44af63a1c87d47f3246
Siyan <sw2936@columbia.edu>
2017-11-14 12:16:17 -0500

    revision

--- unitconvert/tests/test_unitconvert.py
+++ unitconvert/tests/test_unitconvert.py
@@ -9,27 +9,26 @@ def test_unitconvert():
     assert fahrenheit_to_celsius(32.0) == 0
     assert celsius_to_fahrenheit(-32.0* 5.0/9.0) == 0
 
-    # check temperature when fahrenheit equals to celsius
+    # check temperature when fahrenheit equals to celsius at -40, they supposed to equal to each other
     Tf=-40.0
     Tc=-40.0
     Tc_test = fahrenheit_to_celsius(Tf)
     Tf_test = celsius_to_fahrenheit(Tc)
     
+    np.testing.assert_allclose(Tc_test,Tf_test, rtol=1e-5)
     
+    # check conversion between miles and kilometers with some known results
     dm=2
     dk=100
     dk_test=miles_to_kilometers(dm)
     dm_test=kilometers_to_miles(dk)
 
-    # an approximate version of the above
-    np.testing.assert_allclose(Tc_test,Tf_test, rtol=1e-5)
-    
     np.testing.assert_allclose(dk_test,3.218688 , rtol=1e-5)
     np.testing.assert_allclose(dm_test, 62.137119224, rtol=1e-5)
 
     # now check that we can't pass the wrong number of arguments
     with pytest.raises(TypeError):
-        fahrenheit_to_celsius(1, 2, 3, 4)
+        fahrenheit_to_celsius(1, 2)
     with pytest.raises(TypeError):   
         celsius_to_fahrenheit(1, 2)
     with pytest.raises(TypeError):   
